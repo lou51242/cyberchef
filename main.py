a9d968c6418a7d45164f442464b334c107835fa2
Lou <pprijac4@yahoo.com>
2026-09-24 08:56:09 +0200

    Programme 1 final

--- main.py
+++ main.py
@@ -1,88 +1,46 @@
 # Programme 1
 
-# import base64
-# from Crypto.Cipher import AES
+# from Crypto.PublicKey import RSA
+# from Crypto.Cipher import PKCS1_OAEP
 # from Crypto.Random import get_random_bytes
-# from Crypto.Util.Padding import pad, unpad
+# import base64
+# import os
 
-# class AesGestion:
+
+# class RsaGestion:
 #     def __init__(self):
-#         self.aes_key = None
-#         self.iv = None
+#         print("Construction de la classe")
 
-#     def generate_aes_key(self):
-#         self.aes_key = get_random_bytes(32)  # 256-bit key
+#         self.clefPrive = None
+#         self.clefPublic = None
+#     def __del__(self):
+#         print("Destructeur par défaut du RSA")
 
-#     def encrypt_file(self, input_file, output_file):
-#             self.iv = get_random_bytes(16)
-#             cipher = AES.new(self.aes_key, AES.MODE_CBC, self.iv)
+#     def generation_clef(self, nom_fichier_public, nom_fichier_prive, taille):
+#         key = RSA.generate(taille)
+#         self.clefPrive = key
+#         self.clefPublic = key.publickey()
+
+#         with open(nom_fichier_prive, 'wb') as f:
+#             f.write(key.export_key('PEM'))
+#         print(f"Ecriture clef privée dans {nom_fichier_prive}")
+
+#         with open(nom_fichier_public, 'wb') as f:
+#             f.write(self.clefPublic.export_key('PEM'))
+#         print(f"Ecriture clef publique dans {nom_fichier_public}")
+
+#     def chiffrement_rsa(self, donne_claire):
+#             cipher = PKCS1_OAEP.new(self.clefPublic)
+#             donne_claire_bytes = donne_claire.encode('utf-8')
+#             donne_chiffree = cipher.encrypt(donne_claire_bytes)
+#             return base64.b64encode(donne_chiffree).decode('utf-8')
     
-#             with open(input_file, "rb") as f:
-#                 plaintext = f.read()
-    
-#             ciphertext = cipher.encrypt(pad(plaintext, AES.block_size))
-    
-#             with open(output_file, "wb") as f:
-#                 f.write(self.iv + ciphertext)
-    
-#     def decrypt_file(self, input_file, output_file):
-#             with open(input_file, "rb") as f:
-#                 file_content = f.read()
-    
-#             self.iv = file_content[:16]
-#             ciphertext = file_content[16:]
-    
-#             cipher = AES.new(self.aes_key, AES.MODE_CBC, self.iv)
-#             plaintext = unpad(cipher.decrypt(ciphertext), AES.block_size)
-    
-#             with open(output_file, "wb") as f:
-#                 f.write(plaintext)
+#     def dechiffrement_rsa(self, message_chiffre):
+#             cipher = PKCS1_OAEP.new(self.clefPrive)
+#             donne_chiffree = base64.b64decode(message_chiffre)
+#             donne_claire = cipher.decrypt(donne_chiffree)
+#             return donne_claire.decode('utf-8')
 
 
 # Programme 2
-
-
-# import base64
-# from Crypto.Cipher import AES
-# from Crypto.Random import get_random_bytes
-# from Crypto.Util.Padding import pad, unpad
-
-
-# class AesGestion:
-#     def __init__(self):
-#         self.aes_key = None
-#         self.iv = None
-
-#     def load_aes_key_from_file(self, filename):
-#         with open(filename, "rb") as f:
-#             self.aes_key = f.read()
-#         if len(self.aes_key) != 32:
-#             raise ValueError("Invalid AES-256 key size")
-#     def encrypt_file(self, input_file, output_file):
-#         self.iv = get_random_bytes(16)
-#         cipher = AES.new(self.aes_key, AES.MODE_CBC, self.iv)
-
-#         with open(input_file, "rb") as f:
-#             plaintext = f.read()
-
-#         ciphertext = cipher.encrypt(pad(plaintext, AES.block_size))
-
-#         with open(output_file, "wb") as f:
-#             f.write(self.iv + ciphertext)
-
-#     def decrypt_file(self, input_file, output_file):
-#         with open(input_file, "rb") as f:
-#             file_content = f.read()
-
-#         self.iv = file_content[:16]
-#         ciphertext = file_content[16:]
-
-#         cipher = AES.new(self.aes_key, AES.MODE_CBC, self.iv)
-#         plaintext = unpad(cipher.decrypt(ciphertext), AES.block_size)
-
-#         with open(output_file, "wb") as f:
-#             f.write(plaintext)
-
-# Programme 3
-
 
